5c4c4ebf9339639615c481878cefa18eda511470
Paul <krepa098@gmail.com>
2023-02-27 10:40:00 +0100

    build.sh run prune

--- docker/build.sh
+++ docker/build.sh
@@ -9,6 +9,15 @@ if sudo docker build --no-cache --ssh default=$SSH_KEY -t thrsensor . ; then
     docker save thrsensor | gzip > $IMAGE_FILE
 
     echo "Use 'gunzip -c $IMAGE_FILE | docker load' to restore"
+    
+    # run docker prune
+    read -p "Do you want to run 'docker system prune --all'? (y/N)" -n 1 -r
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+    	# this will also ask for user confirmation
+        sudo docker system prune --all
+    fi
 else
     echo "docker build failed!"
 fi
