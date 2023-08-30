98daf07072e5707899610722ad9db462765571e4
Paul <krepa098@gmail.com>
2023-08-30 17:18:36 +0200

    docker: use simple-http-server

--- docker/build.sh
+++ docker/build.sh
@@ -11,13 +11,15 @@ if sudo docker build --no-cache --ssh default=$SSH_KEY -t thrsensor . ; then
     echo "Use 'gunzip -c $IMAGE_FILE | docker load' to restore"
     
     # run docker prune
-    read -p "Do you want to run 'docker system prune --all'? (y/N)" -n 1 -r
+    read -p "Do you want to run remove the image ('docker image rm thrsensor', this will not remove the generated .tgz)? (y/N)" -n 1 -r
     echo ""
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
     	# this will also ask for user confirmation
-        sudo docker system prune --all
+    	sudo docker image rm thrsensor
     fi
+    echo "Run 'docker image prune'?"
+    sudo docker image prune
 else
     echo "docker build failed!"
 fi
--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -15,4 +15,4 @@ echo "Launching backend..."
 
 # run frontend
 echo "Launching frontend..."
-cd /usr/local/bin/frontend && trunk serve --release --address=$TRUNK_ADDRESS --port=$TRUNK_PORT --no-autoreload
+cd /usr/local/bin/frontend/dist && simple-http-server -i --ip=$TRUNK_ADDRESS --port=$TRUNK_PORT
